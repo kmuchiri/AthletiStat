439359d7925020b7eb5df7b2eab5bfda7a0333cb
kgm <kananagmuchiri@gmail.com>
2026-04-14 14:12:25 +0300

    feat(incomplete): list dataset info

--- utils/row_info.sh
+++ utils/row_info.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 # A script to print row count and file size of generated datasets
-for *.csv
+
+echo '# Dataset Information' > info.txt
+echo "Filename, Row Count, File Size" >> info.txt
+
+for csv_file in find . -name "*.csv";
 do
+    name = $(basename $csv_file)
+    file_size = $(wc -c $csv_file)
+    row_count = $(sed 1d $csv_file | wc -l )
+
+    echo "$name, $row_count, $file_size" >> info.txt
+
+done
+    
+    
